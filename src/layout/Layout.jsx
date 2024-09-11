0b688569df5a154338ce3a7dc6241675336de4fc
Etumnu Chinenye-goodcoderCj <chinenyejuliet2019@gmail.com>
2024-09-11 03:57:08 +0100

    fixing layouts

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,7 +3,6 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Sidebar from "../components/Sidebar";
 import Footer from "../components/Footer";
-import classnames from "classnames";
 
 const Layout = () => {
   return (
@@ -11,15 +10,25 @@ const Layout = () => {
       <div className="bg-white shadow-md z-50 w-[15%] min-h-screen overflow-auto sticky">
         <Sidebar />
       </div>
-      <div className="w-[100%] md:w-[85%]">
-        <div className="flex flex-col min-h-screen ">
-          <Header />
-          <Hero />
-          <main className="flex-1 px-6">
-            <Outlet />
-          </main>
-          <Footer />
+
+      <div className="flex flex-col min-h-screen w-[85%]">
+        <Header />
+        <Hero />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 px-6">
+          <div className="col-span-1 bg-white py-4 px-6 text-sm text-blue-600 text-bold">
+            ETHNOS CYBER LTD.
+          </div>
+          <div className="col-span-1 bg-white py-4 px-6 text-sm text-blue-600 text-bold">
+            YOUR GO TO FOR SECURED SERVICES
+          </div>
+          <div className="col-span-1 bg-white py-4 px-6 text-sm text-blue-600 text-bold">
+            WE HOLD YOU ALL THE WAY
+          </div>
         </div>
+        <main className="flex-1 px-6">
+          <Outlet />
+        </main>
+        <Footer />
       </div>
     </div>
   );
