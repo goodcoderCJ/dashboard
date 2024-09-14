67637a4da29248ee4dbd928bbcd5e6fb0b1fff08
Etumnu Chinenye-goodcoderCj <chinenyejuliet2019@gmail.com>
2024-09-14 09:08:08 +0100

    corrected paginate button arrangement

--- src/components/OverlayModal.jsx
+++ src/components/OverlayModal.jsx
@@ -2,7 +2,7 @@
 const OverlayModal = ({ toggleSidebar }) => {
   return (
     <div
-      className="fixed top-0 left-0 right-0 bottom-0 h-screen bg-black/20 z-40"
+      className="fixed top-0 left-0 right-0 bottom-0 h-screen bg-black/20 z-40 flex-1"
       onClick={toggleSidebar}
     ></div>
   );
